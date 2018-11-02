257a1091e50cabeb05a1c82c7d0d053bab2e385c
Eric Schultz <eric@wwahammy.com>
2018-11-02 15:58:09 -0500

    FIXED PROBLEM!

--- config/environments/ci.rb
+++ config/environments/ci.rb
@@ -1,8 +1,2 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-require File.expand_path('config/environments/test')
-
-Commitchange::Application.configure do
-
-  config.log_level = :info
-
-end
+require File.expand_path('config/environments/development')
--- script/test.sh
+++ script/test.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-npm ci && rake db:create db:structure:load test:prepare 
+(
+echo $HOUDINI_WATCH
+set -e
+set -o pipefail
+export DATABASE_URL=${BUILD_DATABASE_URL:-postgres://admin:password@db/commitchange_development}
+echo $DATABASE_URL
+npm ci && rake db:drop db:create db:structure:load test:prepare && npm run test
+)
