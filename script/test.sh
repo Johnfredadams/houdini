d35adaa47af47b5d0484cc87c1716cc7e5b64498
Eric Schultz <eric@wwahammy.com>
2018-11-02 13:03:42 -0500

    TEST

--- script/test.sh
+++ script/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm ci && rake db:create db:structure:load test:prepare && rspec --pattern spec/\*\*\{,/\*/\*\*\}/\*_spec.rb && npm run build && npx jest
+npm ci && rake db:create db:structure:load test:prepare && rake -v spec
