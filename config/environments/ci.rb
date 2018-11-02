d0bd0405f0f809e87da32216ff398f8a9a874422
Eric Schultz <eric@wwahammy.com>
2018-11-02 16:23:34 -0500

    LESS LOGGING

--- config/environments/ci.rb
+++ config/environments/ci.rb
@@ -1,5 +1,6 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 require File.expand_path('config/environments/development')
 Commitchange::Application.configure do
-  config.log_level = :fatal
+  
+  config.active_record.logger = nil
 end
