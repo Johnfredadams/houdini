6db8e0ca83a9c29c64bfd77cce02fb1133f14d53
Eric Schultz <eric@wwahammy.com>
2018-11-02 16:06:27 -0500

    WWWWWHHHE

--- script/test.sh
+++ script/test.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-(
-echo $HOUDINI_WATCH
-set -e
-set -o pipefail
-export DATABASE_URL=${BUILD_DATABASE_URL:-postgres://admin:password@db/commitchange_development}
-echo $DATABASE_URL
-npm ci && rake db:drop db:create db:structure:load test:prepare && npm run test
-)
+npm ci && rake db:create db:structure:load test:prepare && rake -v spec
